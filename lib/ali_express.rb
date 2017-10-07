93d263362f37cb1e166572ebf1c29d435305fae5
Bran Liang <lby89757@hotmail.com>
2017-10-07 11:49:19 +0800

    Add configuration

--- lib/ali_express.rb
+++ lib/ali_express.rb
@@ -1,5 +1,14 @@
 require "ali_express/version"
 
 module AliExpress
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
 end
+
+require_relative 'ali_express/configuration'
---
+++ lib/ali_express/configuration.rb
@@ -0,0 +1,9 @@
+module ZhimaAuth
+  class Configuration
+    attr_accessor :app_code, :query_url
+
+    def initialize
+      @query_url = 'http://jisukdcx.market.alicloudapi.com/express/query'
+    end
+  end
+end
